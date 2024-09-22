570c9fc5e7b87278ec5dd2563d8be219b1634cb2
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-09-23 01:15:33 +0300

    movieprovider

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import AppRouter from "./router/AppRouter";
 import AuthProvider from "./context/AuthProvider";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
+import { MovieProvider } from "./context/MovieProvider";
 
 const App = () => {
   return (
     <div>
      <AuthProvider>
+     <MovieProvider
+     >
      <AppRouter />
+     </MovieProvider>
+    
      </AuthProvider>
      <ToastContainer/>
     
