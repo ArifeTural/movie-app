6ae6d05ce273ab4b58a425902f69c18a9337ed76
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-09-23 00:17:09 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import AppRouter from "./router/AppRouter";
+import AuthProvider from "./context/AuthProvider";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+
+const App = () => {
+  return (
+    <div>
+     <AuthProvider>
+     <AppRouter />
+     </AuthProvider>
+     <ToastContainer/>
+    
+    </div>
+  );
+};
+
+export default App;
